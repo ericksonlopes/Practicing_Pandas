63462087ff8f02427f827efbc6c9fc98d78fa263
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-13 14:44:16 -0300

    removendo espaços

--- criador_de_dados_csv.py
+++ criador_de_dados_csv.py
@@ -10,10 +10,10 @@ with open('data_inicial.csv', 'w', encoding='utf-8') as arquivo:
     arquivo.write('nome,idade,email,uf,genero_musical,linguagem\n')
 
     for item in range(40):
-        linha = f'{fake.name()}, ' \
-                f'{fake.random.randint(18, 70)}, ' \
-                f'{fake.email()}, ' \
+        linha = f'{fake.name()},' \
+                f'{fake.random.randint(18, 70)},' \
+                f'{fake.email()},' \
                 f'{fake.estado_sigla()},' \
                 f'{random.choice(generos_muscais)},' \
-                f'{random.choice(["Python", "Java"])} \n'
+                f'{random.choice(["Python", "Java"])}\n'
         arquivo.write(linha)
