684c7d07b5c282a5154f150d9068f6df0f675d26
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-13 12:02:51 -0300

    manipulando dados, condições, finalizando criador de dados

--- criador_de_dados_csv.py
+++ criador_de_dados_csv.py
@@ -1,18 +1,17 @@
 import random
-
 from faker import Faker
 
 fake = Faker('pt-br')
 
 generos_muscais = ['Axé', 'Blues', 'Country', 'Eletrônica', 'Forró', 'Funk', 'Gospel', 'Hip Hop', 'Jazz', 'MPB',
-                   'Música clássica', 'Pagode', 'Pop', 'Rap', 'Reggae', 'Rock', 'Samba', 'Sertanejo', ]
+                   'Música clássica', 'Pagode', 'Pop', 'Rap', 'Reggae', 'Rock', 'Samba', 'Sertanejo']
 
 with open('data_inicial.csv', 'w', encoding='utf-8') as arquivo:
     arquivo.write('nome,idade,email,uf,genero_musical,linguagem\n')
 
     for item in range(40):
         linha = f'{fake.name()}, ' \
-                f'{fake.random.randint(18, 60)}, ' \
+                f'{fake.random.randint(18, 70)}, ' \
                 f'{fake.email()}, ' \
                 f'{fake.estado_sigla()},' \
                 f'{random.choice(generos_muscais)},' \
