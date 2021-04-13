5f9fd14ecf04802e372f0a363798500495f0f5ac
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-13 09:50:56 -0300

    finalizando criador de dados e carregando os dados no jupyter

--- criador_de_dados_csv.py
+++ criador_de_dados_csv.py
@@ -1,16 +1,18 @@
 import random
+
 from faker import Faker
 
 fake = Faker('pt-br')
 
-generos_muscais = ['Axé', 'Blues', 'Country', 'Eletrônica', 'Forró', 'Funk', 'Gospel', 'Hip Hop' ]
+generos_muscais = ['Axé', 'Blues', 'Country', 'Eletrônica', 'Forró', 'Funk', 'Gospel', 'Hip Hop', 'Jazz', 'MPB',
+                   'Música clássica', 'Pagode', 'Pop', 'Rap', 'Reggae', 'Rock', 'Samba', 'Sertanejo', ]
 
-with open('data.csv', 'w',  encoding='utf-8') as arquivo:
-    arquivo.write('nome,idade,email,uf,genero musical,linguagem\n')
+with open('data_inicial.csv', 'w', encoding='utf-8') as arquivo:
+    arquivo.write('nome,idade,email,uf,genero_musical,linguagem\n')
 
     for item in range(40):
         linha = f'{fake.name()}, ' \
-                f'{fake.random.randint(18, 100)}, ' \
+                f'{fake.random.randint(18, 60)}, ' \
                 f'{fake.email()}, ' \
                 f'{fake.estado_sigla()},' \
                 f'{random.choice(generos_muscais)},' \
