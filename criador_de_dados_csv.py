b0539b62689c9ff15155a116e70bc3e3c340f7fb
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-13 16:09:58 -0300

    quantidade de coluna por coluna

--- criador_de_dados_csv.py
+++ criador_de_dados_csv.py
@@ -7,13 +7,13 @@ generos_muscais = ['Axé', 'Blues', 'Country', 'Eletrônica', 'Forró', 'Funk', 
                    'Música clássica', 'Pagode', 'Pop', 'Rap', 'Reggae', 'Rock', 'Samba', 'Sertanejo']
 
 with open('data_inicial.csv', 'w', encoding='utf-8') as arquivo:
-    arquivo.write('nome,idade,email,uf,genero_musical,linguagem\n')
+    arquivo.write('nome,idade,email,estado,genero_musical,linguagem\n')
 
-    for item in range(40):
+    for item in range(200):
         linha = f'{fake.name()},' \
                 f'{fake.random.randint(18, 70)},' \
                 f'{fake.email()},' \
-                f'{fake.estado_sigla()},' \
+                f'{fake.estado_nome()},' \
                 f'{random.choice(generos_muscais)},' \
                 f'{random.choice(["Python", "Java"])}\n'
         arquivo.write(linha)
