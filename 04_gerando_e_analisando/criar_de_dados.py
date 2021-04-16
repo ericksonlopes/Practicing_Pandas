2e7f86a5e13aa64e8752285690ec2093dc5fb575
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-16 11:58:33 -0300

    email - removendo alguns dados

--- 04_gerando_e_analisando/criar_de_dados.py
+++ 04_gerando_e_analisando/criar_de_dados.py
@@ -9,15 +9,20 @@ linguagens = ['Java', 'C', 'C++', 'Python', 'C#', 'JavaScript', " Visual Basic .
               'Objective-C', 'Assembly', 'Perl', 'Ruby', 'Delphi / Object Pascal', 'Go', 'Scratch', 'PL/SQL',
               'Visual Basic']
 
-with open('dados.csv', 'w', encoding='utf-8') as arquivo:
+with open('dados.csv', 'w+', encoding='utf-8') as arquivo:
     arquivo.write('name,data_nasc,email,pontos,uf,linguagem,genero_musical\n')
 
-    for linha in range(200):
+    for linha in range(300):
         nome = faker.name()
+
+        email = nome.replace(' ', '').lower() + '@' + faker.email().split('@')[1]
+        if linha % 4 == 0:
+            email = ''
+
         arquivo.write(
             f'{nome},'
             f'{faker.date_of_birth(minimum_age=18, maximum_age=70)},'
-            f"{nome.replace(' ', '').lower() + '@' + faker.email().split('@')[1]},"
+            f"{email},"
             f'{faker.random.randint(0, 100)},'
             f'{faker.estado_sigla()},'
             f'{faker.random.choice(linguagens)},'
